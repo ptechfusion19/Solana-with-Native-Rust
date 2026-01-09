4ae63e65f541c75ff6599a5fcd02ac4d5876b571
abdullah <abdullah.se4th@gmail.com>
2026-01-09 16:21:14 +0500

    Initial Branch

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("GYECCrPEnJguwmiSe28x8T4sT8rBdufmESMC8ZXJBjxr");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
